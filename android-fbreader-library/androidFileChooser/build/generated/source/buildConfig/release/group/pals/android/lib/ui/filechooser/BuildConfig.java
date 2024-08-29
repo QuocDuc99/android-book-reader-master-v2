4e8deec6c83c0731452711460e2907bcdb3fbecb
QuocDuc99 <ducnq@qig.vn>
2024-08-29 16:05:31 +0700

    add jitpack and pom and readbook

---
+++ android-fbreader-library/androidFileChooser/build/generated/source/buildConfig/release/group/pals/android/lib/ui/filechooser/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package group.pals.android.lib.ui.filechooser;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "group.pals.android.lib.ui.filechooser";
+  public static final String BUILD_TYPE = "release";
+}
